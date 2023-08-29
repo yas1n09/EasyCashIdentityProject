8be318b9b6d4462cbd85e56be4ff6a9777631a0b
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2023-08-29 18:17:46 +0300

    Added Register Control. Added 6 digits random confirmation code Added "Sending Confirmation Code to the Register Mail"

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -1,53 +1,82 @@
 ﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
 using EasyCashIdentityProject.EntityLayer.Concrete;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 
 namespace EasyCashIdentityProject.PresentationLayer.Controllers
 {
-    public class RegisterController : Controller
-    {
-        private readonly UserManager<AppUser> _userManager;
+	public class RegisterController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
 
-        public RegisterController(UserManager<AppUser> userManager)
-        {
-            _userManager = userManager;
-        }
-        [HttpGet]
-        public IActionResult Index()
-        {
-            return View();
-        }
-        [HttpPost]
-        public async Task <IActionResult> Index(AppUserRegisterDto appUserRegisterDto) 
-        { 
-            if (ModelState.IsValid)
-            {
-                AppUser appUser = new AppUser()
-                {
-                    UserName = appUserRegisterDto.Username,
-                    Name = appUserRegisterDto.Name,
-                    Surname = appUserRegisterDto.Surname,
-                    Email = appUserRegisterDto.Email,
-                    City="AAA",
-					District="BBBB",
-                    ImageUrl="CCCc"
+		public RegisterController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+		{
+			if (ModelState.IsValid)
+			{
+				Random random = new Random();
+				int code;
+				code = random.Next(100000, 1000000);
 
+
+				AppUser appUser = new AppUser()
+				{
+					UserName = appUserRegisterDto.Username,
+					Name = appUserRegisterDto.Name,
+					Surname = appUserRegisterDto.Surname,
+					Email = appUserRegisterDto.Email,
+					City = "AAA",
+					District = "BBBB",
+					ImageUrl = "CCCc",
+					ConfirmCode = code
 				};
-                var result = await _userManager.CreateAsync(appUser,appUserRegisterDto.Password);
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("Index","ConfirmMail");
-                }
-                else
-                {
-                    foreach(var item in result.Errors)
-                    {
-                        ModelState.AddModelError("", item.Description);
-                    }
-                }
-            }
-            return View();
-        }
-    }
+				var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+				if (result.Succeeded)
+				{
+					MimeMessage mimeMessage = new MimeMessage();
+					MailboxAddress mailboxAddressFrom = new MailboxAddress("EasyCash Admin", "EasyCashMailService@gmail.com");
+					MailboxAddress mailboxAddressTo = new MailboxAddress("User", appUser.Email);
+
+					mimeMessage.From.Add(mailboxAddressFrom);
+					mimeMessage.To.Add(mailboxAddressTo);
+
+					var bodyBuilder = new BodyBuilder();
+					bodyBuilder.TextBody = "Code For Registration: " + code;
+					mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+					mimeMessage.Subject = "EasyCash Registration Code";
+
+					SmtpClient client = new SmtpClient();
+					client.Connect("smtp.gmail.com", 587, false);
+					client.Authenticate("EasyCashMailService@gmail.com", "mlgdztqdqkmddmzm");
+					client.Send(mimeMessage);
+					client.Disconnect(true);
+
+					TempData["Mail"] = appUserRegisterDto.Email;
+
+
+					return RedirectToAction("Index", "ConfirmMail");
+				}
+				else
+				{
+					foreach (var item in result.Errors)
+					{
+						ModelState.AddModelError("", item.Description);
+					}
+				}
+			}
+			return View();
+		}
+	}
 }
